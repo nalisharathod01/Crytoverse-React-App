42fd739be1104fe53f41a5a633cbf59b2fff6478
Nalisha Rathod <rathodnalisha92@gmail.com>
2023-10-03 15:57:07 -0400

    added CryptoVerse React App

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,30 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Key': '2c94fdfb12msh251ba9647ea634dp156981jsn227cc101c964',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
+
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+    reducerPath:'cryptoApi',
+    baseQuery:fetchBaseQuery({baseUrl}),
+    endpoints:(builder) =>({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`/coins?limit=${count}`),
+          }),
+          getCryptoDetails: builder.query({
+            query: (coinId) => createRequest(`/coin/${coinId}`),
+          }),
+          getCryptoHistory: builder.query({
+            query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
+          }),
+      
+    })
+})
+
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } = cryptoApi
